a91eaca0dcc8932a47e23ba280f4eccd78b44a31
Grigory Taran <grunder-96@mail.ru>
2025-03-06 11:07:35 +0300

    feature: add AppConfig

---
+++ src/main/java/org/edu/pet/config/AppConfig.java
@@ -0,0 +1,10 @@
+package org.edu.pet.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.edu.pet")
+public class AppConfig {
+
+}
