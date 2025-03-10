c4f6f0615d317b1a284833a7d30afd5d3b32f9e7
Grigory Taran <grunder-96@mail.ru>
2025-03-10 17:27:14 +0300

    feature: add liquibase bean, move datasource bean to AppConfig, move common properties to application.properties

--- src/main/java/org/edu/pet/config/HibernateConfig.java
+++ src/main/java/org/edu/pet/config/HibernateConfig.java
@@ -2,9 +2,11 @@ package org.edu.pet.config;
 
 import lombok.RequiredArgsConstructor;
 import org.hibernate.SessionFactory;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -37,24 +39,9 @@ public class HibernateConfig {
         return sessionFactory;
     }
 
-    @Bean
-    public DataSource dataSource() {
-
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-
-        dataSource.setDriverClassName(env.getRequiredProperty("hibernate.connection.driver_class"));
-        dataSource.setUrl(env.getRequiredProperty("hibernate.connection.url"));
-        dataSource.setUsername(env.getRequiredProperty("hibernate.connection.username"));
-        dataSource.setPassword(env.getRequiredProperty("hibernate.connection.password"));
-
-        return dataSource;
-    }
-
     private Properties getHibernateProperties() {
-
         Properties properties = new Properties();
 
-        properties.put("hibernate.connection.provider_class", env.getRequiredProperty("hibernate.connection.provider_class"));
         properties.put("hibernate.current_session_context_class", env.getRequiredProperty("hibernate.current_session_context_class"));
         properties.put("hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"));
         properties.put("hibernate.format_sql", env.getRequiredProperty("hibernate.format_sql"));
