7dccf377dde8898add53edef56d8c1e06e4d5d53
Grigory Taran <grunder-96@mail.ru>
2025-03-20 15:34:34 +0300

    feature: add AuthInterceptor

---
+++ src/main/java/org/edu/pet/interceptor/AuthInterceptor.java
@@ -0,0 +1,67 @@
+package org.edu.pet.interceptor;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.edu.pet.exception.InvalidSessionIdException;
+import org.edu.pet.exception.SessionNotFoundException;
+import org.edu.pet.model.UserSession;
+import org.edu.pet.service.SessionService;
+import org.edu.pet.util.CookieUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.util.WebUtils;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class AuthInterceptor implements HandlerInterceptor {
+
+    @Value("${cookie.session_id.name}")
+    private String sessionCookieName;
+
+    private final SessionService sessionService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
+
+        if (req.getRequestURI().startsWith("/signin")) {
+            return true;
+        }
+
+        Cookie cookie = WebUtils.getCookie(req, sessionCookieName);
+
+        if (cookie == null) {
+            resp.sendRedirect(req.getContextPath() + "/signin");
+            return false;
+        }
+
+        UUID sessionId = convertToSessionId(cookie.getValue());
+
+        UserSession session = sessionService
+                .getSession(sessionId)
+                .orElseThrow(() -> {
+                    resp.addCookie(CookieUtil.delete(cookie.getName()));
+                    return new SessionNotFoundException();
+                });
+
+        if (sessionService.isSessionExpired(session)) {
+            resp.addCookie(CookieUtil.delete(cookie.getName()));
+            throw new InvalidSessionIdException();
+        }
+
+        req.setAttribute("user", session.getUser());
+        return true;
+    }
+
+    private UUID convertToSessionId(String uuidAsString) {
+        try {
+            return UUID.fromString(uuidAsString);
+        } catch (IllegalArgumentException e) {
+            throw new InvalidSessionIdException();
+        }
+    }
+}
