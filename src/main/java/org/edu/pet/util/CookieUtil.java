9db028c6e4d2e460dcb417ef6b25297749c96944
Grigory Taran <grunder-96@mail.ru>
2025-04-14 15:24:04 +0300

    feature: add CookieUtil

--- src/main/java/org/edu/pet/util/CookieUtil.java
+++ src/main/java/org/edu/pet/util/CookieUtil.java
@@ -1,16 +1,33 @@
 package org.edu.pet.util;
 
 import jakarta.servlet.http.Cookie;
+import lombok.NonNull;
 import lombok.experimental.UtilityClass;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 @UtilityClass
 public class CookieUtil {
 
     public Cookie delete(String cookieName) {
 
-        Cookie cookie = new Cookie(cookieName, null);
+        return create(cookieName, null, 0);
+    }
+
+    public Cookie create(String cookieName, String cookieValue, @NonNull LocalDateTime expiresAt) {
+
+        Duration between = Duration.between(LocalDateTime.now(), expiresAt);
+
+        return between.isPositive() ? create(cookieName, cookieValue, (int) between.getSeconds()) :
+                delete(cookieName);
+    }
+
+    private Cookie create(String cookieName, String cookieValue, int maxAge) {
+
+        Cookie cookie = new Cookie(cookieName, cookieValue);
         cookie.setPath("/");
-        cookie.setMaxAge(0);
+        cookie.setMaxAge(maxAge);
         cookie.setHttpOnly(true);
 
         return cookie;
