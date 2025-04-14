5f355b3503343a8331fa31e59ba0ceb9cfe10bda
Grigory Taran <grunder-96@mail.ru>
2025-04-14 16:38:30 +0300

    feature: add UserRepository

--- src/main/java/org/edu/pet/repository/UserRepository.java
+++ src/main/java/org/edu/pet/repository/UserRepository.java
@@ -4,7 +4,10 @@ import org.edu.pet.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
+    Optional<User> findByLoginIgnoreCase(String login);
 }
