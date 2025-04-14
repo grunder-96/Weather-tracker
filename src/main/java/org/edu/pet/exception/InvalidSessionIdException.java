5e1e81275420707f7b8ec9cab58398140e9dd058
Grigory Taran <grunder-96@mail.ru>
2025-04-14 15:31:59 +0300

    refactor: add constructor with custom message to exception

--- src/main/java/org/edu/pet/exception/InvalidSessionIdException.java
+++ src/main/java/org/edu/pet/exception/InvalidSessionIdException.java
@@ -2,4 +2,11 @@ package org.edu.pet.exception;
 
 public class InvalidSessionIdException extends RuntimeException {
 
+    public InvalidSessionIdException() {
+        super("The value is not a session identifier.");
+    }
+
+    public InvalidSessionIdException(String message) {
+        super(message);
+    }
 }
--- src/main/java/org/edu/pet/exception/SessionNotFoundOrExpiredException.java
+++ src/main/java/org/edu/pet/exception/SessionNotFoundOrExpiredException.java
@@ -2,4 +2,11 @@ package org.edu.pet.exception;
 
 public class SessionNotFoundOrExpiredException extends RuntimeException{
 
+    public SessionNotFoundOrExpiredException() {
+        super("Session not found or expired.");
+    }
+
+    public SessionNotFoundOrExpiredException(String message) {
+        super(message);
+    }
 }
