8c04e4c8108e732c9523279ead651a9f325f6e75
Grigory Taran <grunder-96@mail.ru>
2025-04-29 09:37:22 +0300

    refactor: remove unused constant

--- src/main/java/org/edu/pet/constant/TemplateNames.java
+++ src/main/java/org/edu/pet/constant/TemplateNames.java
@@ -8,4 +8,5 @@ public class TemplateNames {
     public final String SIGN_IN = "sign-in";
     public final String SIGN_UP = "sign-up";
     public final String MAIN = "main";
+    public final String SEARCH = "search";
 }
