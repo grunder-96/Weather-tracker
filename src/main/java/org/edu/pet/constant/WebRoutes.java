fb7f0590f9ec62660daa7d862da97366261c5eb3
Grigory Taran <grunder-96@mail.ru>
2025-04-29 17:49:50 +0300

    feature: add missing web-routes

--- src/main/java/org/edu/pet/constant/WebRoutes.java
+++ src/main/java/org/edu/pet/constant/WebRoutes.java
@@ -7,11 +7,21 @@ import lombok.experimental.UtilityClass;
 public class WebRoutes {
 
     private final String REDIRECT = "redirect:";
+    private final String ALL = "/**";
+
+    public final String ADD = "/add";
+    public final String DELETE = "/delete";
 
     public final String SIGN_UP = "/signup";
     public final String SIGN_IN = "/signin";
     public final String SIGN_OUT = "/signout";
     public final String MAIN = "/";
+    public final String SEARCH = "/search";
+    public final String LOCATION = "/location";
+    public final String STATIC = "/static";
+
+    public final String LOCATION_ALL = LOCATION + ALL;
+    public final String STATIC_ALL = STATIC + ALL;
 
     public String redirectTo(@NonNull String route) {
         return REDIRECT + route;
