ce3dd0dcdd6be8c63cc3850544fbe398b3bc82f0
Grigory Taran <grunder-96@mail.ru>
2025-04-29 10:59:09 +0300

    feature: add AbstractApiClient

---
+++ src/main/java/org/edu/pet/service/client/AbstractApiClient.java
@@ -0,0 +1,29 @@
+package org.edu.pet.service.client;
+
+import lombok.RequiredArgsConstructor;
+import org.edu.pet.exception.ApiClientException;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@RequiredArgsConstructor
+public abstract class AbstractApiClient {
+
+    final HttpClient httpClient;
+    final String baseUrl;
+
+    @Value("${api.key}")
+    String apiKey;
+
+    <T> HttpResponse<T> getResponse(HttpRequest req, HttpResponse.BodyHandler<T> responseBodyHandler) {
+
+        try {
+            return httpClient.send(req, responseBodyHandler);
+        } catch (IOException | InterruptedException e) {
+            throw new ApiClientException();
+        }
+    }
+}
