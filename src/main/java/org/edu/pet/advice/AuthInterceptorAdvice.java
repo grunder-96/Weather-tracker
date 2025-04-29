20379bab924c842dc98847d99e40411d5920fe1e
Grigory Taran <grunder-96@mail.ru>
2025-04-29 12:15:59 +0300

    refactor: now use constant utility class

--- src/main/java/org/edu/pet/advice/AuthInterceptorAdvice.java
+++ src/main/java/org/edu/pet/advice/AuthInterceptorAdvice.java
@@ -1,11 +1,11 @@
 package org.edu.pet.advice;
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.edu.pet.constant.SessionCookieSettings;
 import org.edu.pet.constant.WebRoutes;
 import org.edu.pet.exception.InvalidSessionIdException;
 import org.edu.pet.exception.SessionNotFoundOrExpiredException;
 import org.edu.pet.util.CookieUtil;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -13,13 +13,10 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @ControllerAdvice
 public class AuthInterceptorAdvice {
 
-    @Value("${custom.session.cookie.name}")
-    private String customSessionCookieName;
-
     @ExceptionHandler({SessionNotFoundOrExpiredException.class, InvalidSessionIdException.class})
     public String handleAuthInterceptorExceptions(RedirectAttributes redirectAttributes, HttpServletResponse resp) {
 
-        resp.addCookie(CookieUtil.delete(customSessionCookieName));
+        resp.addCookie(CookieUtil.delete(SessionCookieSettings.COOKIE_NAME));
         redirectAttributes.addFlashAttribute("errorNotification", "Session not found or expired. Please log in again.");
         return WebRoutes.redirectTo(WebRoutes.SIGN_IN);
     }
--- src/main/java/org/edu/pet/interceptor/UnauthInterceptor.java
+++ src/main/java/org/edu/pet/interceptor/UnauthInterceptor.java
@@ -4,8 +4,8 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.edu.pet.constant.SessionCookieSettings;
 import org.edu.pet.constant.WebRoutes;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.util.WebUtils;
@@ -14,13 +14,10 @@ import org.springframework.web.util.WebUtils;
 @RequiredArgsConstructor
 public class UnauthInterceptor implements HandlerInterceptor {
 
-    @Value("${custom.session.cookie.name}")
-    private String customSessionCookieName;
-
     @Override
     public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
 
-        Cookie cookie = WebUtils.getCookie(req, customSessionCookieName);
+        Cookie cookie = WebUtils.getCookie(req, SessionCookieSettings.COOKIE_NAME);
 
         if (cookie == null) {
             return true;
