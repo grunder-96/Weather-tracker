5ed6a6f1a7865d48166b228fcacdcc04f396176e
Grigory Taran <grunder-96@mail.ru>
2025-04-29 12:12:20 +0300

    refactor: add remove method, improve logic

--- src/main/java/org/edu/pet/service/SessionService.java
+++ src/main/java/org/edu/pet/service/SessionService.java
@@ -1,14 +1,14 @@
 package org.edu.pet.service;
 
 import lombok.RequiredArgsConstructor;
+import org.edu.pet.constant.SessionCookieSettings;
 import org.edu.pet.dto.resp.SessionResponseDto;
+import org.edu.pet.exception.InvalidSessionIdException;
 import org.edu.pet.mapper.SessionMapper;
 import org.edu.pet.model.User;
 import org.edu.pet.model.UserSession;
 import org.edu.pet.repository.SessionRepository;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
@@ -17,35 +17,49 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class SessionService {
-
-    @Value("${custom.session.expiration.days}")
-    private int daysUntilExpiration;
 
     private final SessionRepository sessionRepository;
     private final SessionMapper sessionMapper;
 
+    @Transactional
     UserSession createInternal(User user) {
 
         UserSession session = UserSession.builder()
                 .user(user)
-                .expiresAt(LocalDateTime.now().plusDays(daysUntilExpiration))
+                .expiresAt(LocalDateTime.now()
+                        .plusSeconds(SessionCookieSettings.LIFETIME_SECONDS))
                 .build();
 
         return sessionRepository.save(session);
     }
 
     @Transactional(readOnly = true)
-    public Optional<SessionResponseDto> get(UUID sessionId) {
+    public Optional<SessionResponseDto> get(String uuidAsString) {
 
+        UUID sessionId = convertToUuid(uuidAsString);
         Optional<UserSession> sessionOptional = sessionRepository.findById(sessionId);
-        return sessionOptional.map(sessionMapper::toDto);
+        return sessionOptional.map(sessionMapper::toSessionResponseDto);
     }
 
-    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    @Transactional
+    public void remove(String uuidAsString) {
+
+        UUID sessionId = convertToUuid(uuidAsString);
+        sessionRepository.deleteById(sessionId);
+    }
+
     public boolean isSessionExpired(SessionResponseDto sessionResponseDto) {
 
         return LocalDateTime.now().isAfter(sessionResponseDto.expiresAt());
+    }
+
+    private UUID convertToUuid(String uuidAsString) {
+
+        try {
+            return UUID.fromString(uuidAsString);
+        } catch (IllegalArgumentException e) {
+            throw new InvalidSessionIdException();
+        }
     }
 }
