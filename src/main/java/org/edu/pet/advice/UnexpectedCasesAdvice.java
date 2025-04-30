06e7876f4f4538e209b11bae410c490776214bfe
Grigory Taran <grunder-96@mail.ru>
2025-04-30 14:53:58 +0300

    refactor: edit exception type for UnexpectedCasesAdvice

--- src/main/java/org/edu/pet/advice/UnexpectedCasesAdvice.java
+++ src/main/java/org/edu/pet/advice/UnexpectedCasesAdvice.java
@@ -1,14 +1,17 @@
 package org.edu.pet.advice;
 
 import org.edu.pet.constant.TemplateNames;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class UnexpectedCasesAdvice {
 
-    @ExceptionHandler(Exception.class)
-    public String handleUnexpectedCase() {
+    @ExceptionHandler(RuntimeException.class)
+    public String handleUnexpectedCase(Exception e) {
 
         return TemplateNames.ERROR;
     }
